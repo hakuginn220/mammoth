c1295325101af15905737976f8b8d885f8f96908
hakuginn220 <hakuginn220@white-falls.com>
2017-04-14 15:05:22 +0900

    move directory

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'app/index.html'),
+    pathname: path.join(__dirname, 'view/index.html'),
     protocol: 'file:',
     slashes: true
   }))
