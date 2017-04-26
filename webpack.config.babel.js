e8595c3d8dc34b034e65b411f7970ed2530648a1
hakuginn220 <hakuginn220@white-falls.com>
2017-04-26 17:00:07 +0900

    fix webpack config es2015

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,4 +1,4 @@
-const path = require('path')
+import path from 'path'
 
 const option = {
   output: {
@@ -24,17 +24,13 @@ const option = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'es2015']
-        }
+        loader: 'babel-loader'
       }
     ]
   }
 }
 
-module.exports = [
-
+export default [
   Object.assign({}, option, {
     entry: {
       index: path.join(__dirname, 'src/index/index.js')
@@ -51,5 +47,4 @@ module.exports = [
       __dirname: false
     }
   })
-
 ]
