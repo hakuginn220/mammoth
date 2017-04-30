afed0e0fabdec660d8bd63df231bc5ce01e3ec46
hakuginn220 <hakuginn220@white-falls.com>
2017-04-30 17:45:57 +0900

    add fetch api

--- src/main/utils/oauth.js
+++ src/main/utils/oauth.js
@@ -1,33 +1,23 @@
-import { ipcMain, net } from 'electron'
+import url from 'url'
+import fetch from 'isomorphic-fetch'
+import { ipcMain } from 'electron'
 import * as Actions from '../actions/oauth'
-
-const submitOauth = (value) => {
-  return new Promise((resolve, reject) => {
-    const request = net.request({
-      method: 'GET',
-      protocol: 'https:',
-      hostname: value.hostname,
-      port: 443,
-      path: '/api/v1/instance'
-    })
-    request.on('error', (error) => {
-      reject(error)
-    })
-    request.on('response', (response) => {
-      response.on('data', (data) => {
-        resolve(data)
-      })
-    })
-    request.end()
-  })
-}
 
 export default () => {
   ipcMain.on(Actions.SUBMIT_OAUTH, (event, value) => {
-    submitOauth(value)
-    .then((data) => {
-      console.log(JSON.parse(data))
-      event.sender.send(Actions.SUBMIT_OAUTH_SUCCESS, JSON.parse(data))
+    const api = url.format({
+      protocol: 'https:',
+      hostname: value.hostname,
+      pathname: 'api/v1/instance'
+    })
+
+    fetch(api)
+    .then((response) => {
+      return response.json()
+    })
+    .then((stories) => {
+      console.log(stories)
+      event.sender.send(Actions.SUBMIT_OAUTH_SUCCESS, stories)
     })
     .catch((error) => {
       console.log(error)
