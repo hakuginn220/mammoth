e0416106d40b7f44daf3d7392014e51f70c1bf7f
hakuginn220 <hakuginn220@white-falls.com>
2017-06-27 18:29:58 +0900

    fix main

--- src/main/index.js
+++ src/main/index.js
@@ -1,30 +1,5 @@
-import path from 'path'
-import url from 'url'
-import { app, BrowserWindow } from 'electron'
-
-let win
-
-function createWindow () {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minWidth: 320,
-    minHeight: 480,
-    icon: path.join(__dirname, 'images/icon.ico'),
-    backgroundColor: '#282c37',
-    darkTheme: true
-  })
-
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  win.on('closed', () => {
-    win = null
-  })
-}
+import { app } from 'electron'
+import { createWindow } from './browser'
 
 app.on('ready', () => {
   createWindow()
@@ -37,7 +12,5 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+  createWindow()
 })
